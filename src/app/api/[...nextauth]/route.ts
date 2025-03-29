caccbfb271b6c81754629317f0eb3e5e916c9e64
apoorv0220 <apoorv0220@gmail.com>
2025-03-29 12:52:18 +0530

    Invalid route export fix

--- src/app/api/[...nextauth]/route.ts
+++ src/app/api/[...nextauth]/route.ts
@@ -1,37 +1,36 @@
-import NextAuth, { AuthOptions } from "next-auth"
+import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { JWT } from "next-auth/jwt"
 import { Session, User } from "next-auth"
+import type { NextAuthOptions } from "next-auth"
 
 
-export const getAuthOptions = ():AuthOptions => {
-  return {
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-      })
-    ],
-    session: {
-      strategy: "jwt",
-    },
-    callbacks: {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      async jwt({ token, account }: { token: JWT, account: any }) {
-        if (account) {
-          token.accessToken = account.access_token
-        }
-        return token
-      },
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async session({ session, token, user }: { session: Session, token: JWT, user: User }) {
-        // session.accessToken = token.accessToken
-        return session
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, account }: { token: JWT, account: any }) {
+      if (account) {
+        token.accessToken = account.access_token
       }
+      return token
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async session({ session, token, user }: { session: Session, token: JWT, user: User }) {
+      // session.accessToken = token.accessToken
+      return session
     }
   }
 }
 
-const handler = NextAuth(getAuthOptions())
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
