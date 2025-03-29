e8c2014698d048a7a64eb661d15c77d33b04b650
apoorv0220 <apoorv0220@gmail.com>
2025-03-30 00:33:11 +0530

    Add content security policy

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com/gsi/; style-src 'self' 'unsafe-inline'; img-src 'self' data: res.cloudinary.com; font-src 'self';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
